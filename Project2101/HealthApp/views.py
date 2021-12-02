6c5426a3ac133950ebaba6bf7dfe7b9586c78cdf
LL753 <ll753@live.mdx.ac.uk>
2021-12-02 09:31:02 +0400

    Login page test

--- Project2101/HealthApp/views.py
+++ Project2101/HealthApp/views.py
@@ -8,4 +8,23 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    if not request.user.is_authenticated:
+        return render(request,"login.html",{"message": None})
+    message = {
+        "message":"Logged in Yay"
+    }
+    return render(request,"index.html",message)
+
+def logout_view(request):
+    logout(request)
+    return render(request,"login.html",{"message":"Logged out"})
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "login.html", {"message": "Invalid credentials."})
