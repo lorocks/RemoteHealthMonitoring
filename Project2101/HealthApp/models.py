5288f8f421e8b6166718ef26ce080a38dcfb0caa
LL753 <ll753@live.mdx.ac.uk>
2021-12-22 20:03:40 +0400

    models added ig

--- Project2101/HealthApp/models.py
+++ Project2101/HealthApp/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UserIDs(models.Model):
+    username = models.CharField(max_length = 4, primary_key = True)
+
+class UserType(models.Model):
+    Type = [("Doctor"), ("Patient")]
+    userID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, primary_key = True)
+    username = models.ForeignKey(UserIDs, on_delete = models.CASCADE)
+    userType = models.CharField(max_length = 7, choices = Type, null = False)
+
+
+class Doctors(models.Model):
+    DoctorID = models.ForeignKey(UserIDs, on_delete = models.CASCADE, primary_key = True)
+    Name = models.CharField(max_length = 100, null = False, blank = False)
+    ContactNum = models.CharField(max_length = 12, null = False, blank = False) #do num validation in front end, add on_delete
+    Hospital = models.CharField(max_length = 100, null = False, blank = False)
+
+class Patients(models.Model):
+    GenderChoices = [("F"), ("M")]
+    PatientID = models.ForeignKey(UserIDs, on_delete = models.CASCADE, primary_key = True)
+    DoctorID = models.ForeignKey(Doctors, on_delete = models.CASCADE, null = False, blank = False)
+    Name = models.CharField(max_length=100, null=False, blank=False)
+    DOB = models.DateField(null=False, blank=False)
+    Gender = models.CharField(max_length = 1, choices = GenderChoices)
+    Address = models.CharField(max_length = 150, null=False, blank=False)
+    ContactNum = models.CharField(max_length = 12, null = False, blank = False)
+    EmergencyContact = models.CharField(max_length = 12, null = False, blank = False)
+    Insurance = models.CharField(max_length = 50)
+    EmailID = models.CharField(max_length = 100)
+
+class DailyData(models.Model):
+    PatientID = models.ForeignKey(Patients, on_delete = models.CASCADE)
+    Date = models.DateField(null=False, blank=False)
+    LastAvgTemp = models.FloatField()    #fill when person enters sugar lvl, by run procedure ig
+    LastAvgHeartRate = models.FloatField()   #fill when person enters sugar lvl, by run procedure ig of previous day
+    BloodSugar = models.FloatField()
+    BloodPressure = models.FloatField()
+#gotta run procedure for auto delete ig
+class SecondlyData(models.Models):
+    PatientID = models.ForeignKey(Patients, on_delete = models.CASCADE)
+    Date = models.DateField(null=False, blank=False)
+    Time = models.TimeField(null=False, blank=False)
+    Pulse = models.FloatField(null=False, blank=False)
+    Temp = models.FloatField(null=False, blank=False)
+
+class WeeklyData(models.Model): # or monthly idk
+    PatientID = models.ForeignKey(Patients, on_delete=models.CASCADE)
+    Date = models.DateField(null=False, blank=False)
+    Weight = models.FloatField()
+    Height = models.FloatField()
+    BMI = models.FloatField()
